bf087b69360e74d3ed11fbba0f0aff715adf3ceb
Kajlax <32290883+Kajlax@users.noreply.github.com>
2018-10-18 10:26:35 +0300

    Added get for single id-number to backend

--- server.js
+++ server.js
@@ -7,4 +7,10 @@ app.get("/api/bears", (req, res) => {
   res.json(bears);
 });
 
+app.get("/api/bears/:id", (req, res) => {
+  const bear = bears.find(c => c.id === parseInt(req.params.id));
+  if (!bear) res.status(404).send("Id not found");
+  res.send(bear);
+});
+
 app.listen(port, () => console.log(`Server started, port ${port}`));
